cc3cf133a881d5736613c11646eb08353bb7c163
kreditkutim <m.muslihsgt@gmail.com>
2025-09-08 15:14:29 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("syariah-cache-v1").then((cache) => {
+      return cache.addAll([
+        "/syariah/data.html",
+        "/syariah/manifest.json",
+        "/syariah/icon-192.png",
+        "/syariah/icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
