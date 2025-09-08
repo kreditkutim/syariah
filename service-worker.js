69067702f9937718768d7977ec1d0ae5dd008925
kreditkutim <m.muslihsgt@gmail.com>
2025-09-08 15:33:44 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,10 +2,10 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open("syariah-cache-v1").then((cache) => {
       return cache.addAll([
-        "/syariah/data.html",
-        "/syariah/manifest.json",
-        "/syariah/icon-192.png",
-        "/syariah/icon-512.png"
+        "/data.html",
+        "/manifest.json",
+        "/icon-192.png",
+        "/icon-512.png"
       ]);
     })
   );
