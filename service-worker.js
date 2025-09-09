44e395f52d166e1320b5cbaccac8a9e5fc87e9ba
kreditkutim <m.muslihsgt@gmail.com>
2025-09-09 12:50:05 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,54 @@
-self.addEventListener("install", (event) => {
+const CACHE_NAME = 'kredit-kutim-v1';
+const urlsToCache = [
+  '/syariah/',
+  '/syariah/data.html',
+  '/syariah/style.css',
+  '/syariah/script.js',
+  '/syariah/icon-192x192.png',
+  '/syariah/icon-512x512.png'
+];
+
+// Install service worker
+self.addEventListener('install', event => {
+  console.log('Service worker installing...');
+  // Perform install steps
   event.waitUntil(
-    caches.open("syariah-cache-v1").then((cache) => {
-      return cache.addAll([
-        "./data.html",
-        "./manifest.json",
-        "./icon-192.png",
-        "./icon-512.png"
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener("fetch", (event) => {
+// Cache and return requests
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  console.log('Service worker activating...');
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
